74a1ac04d560b3f14f946edd8d0a80ab2135240a
Bui Gia An <buigiaanfb1@gmail.com>
2022-05-22 01:36:27 +0700

    [An] Config Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import Counter from './features/counter/Counter';
-import Pokemon from './features/pokemon/Pokemon';
+import { routes } from 'routes';
+import { useRoutes } from 'react-router-dom';
 
-function App() {
+const App: React.FC = () => {
+  // The useRoutes() hook allows you to define your routes as JavaScript objects
+  // instead of <Routes> and <Route> elements. This is really just a style
+  // preference for those who prefer to not use JSX for their routes config.
+  let element = useRoutes(routes);
+
   return (
-      <>
-       <h1>DTO team</h1>
-       <Counter/>
-       <Pokemon/>
-      </>
+    <>
+      <h1>DTO team</h1>
+      {element}
+    </>
   );
-}
+};
 
 export default App;
